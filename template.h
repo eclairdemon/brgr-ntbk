810812d8fb7751b75ec4e0d0d5c4f241e19456a4
eclairdemon <maddie.fca@gmail.com>
2026-07-18 13:09:00 +0800

    Add template file

---
+++ template.h
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+using vec = vector<T>;
+using ll = long long;
+using vll = vec<ll>;
+using vvll = vec<vll>;
+using pll = pair<ll,ll>;
+using str = string;
+using ld = long double;
+using cd = complex<double>;
+
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998'244'353;
+const ld PI = acos(-1);
